6e201ea9102f5de633d51b58b8898ecf79a50d65
YuNaga224 <nagasaki.y24@gmail.com>
2023-03-20 00:38:15 +0900

    コメントを追加!

--- board/board.go
+++ board/board.go
@@ -8,8 +8,10 @@ type Board struct {
 	Width  int
 	Height int
 	Cells  [][]bool
+	Score int
 }
 
+// 新しいゲームボードを生成する関数
 func NewBoard(width, height int) *Board {
 	cells := make([][]bool, height)
 	for i := range cells {
@@ -23,6 +25,7 @@ func NewBoard(width, height int) *Board {
 	}
 }
 
+// テトリミノを左に移動できるかを判定する関数
 func (b *Board) CanMoveLeft(t *tetrimino.Tetrimino) bool {
 	for _, cell := range t.Cells() {
 		x, y := cell.X-1, cell.Y
@@ -33,6 +36,7 @@ func (b *Board) CanMoveLeft(t *tetrimino.Tetrimino) bool {
 	return true
 }
 
+// テトリミノを右に移動できるかを判定する関数
 func (b *Board) CanMoveRight(t *tetrimino.Tetrimino) bool {
 	for _, cell := range t.Cells() {
 		x, y := cell.X+1, cell.Y
@@ -43,6 +47,7 @@ func (b *Board) CanMoveRight(t *tetrimino.Tetrimino) bool {
 	return true
 }
 
+// テトリミノを下に移動できるかを判定する関数
 func (b *Board) CanMoveDown(t *tetrimino.Tetrimino) bool {
 	for _, cell := range t.Cells() {
 		x, y := cell.X, cell.Y+1
@@ -53,6 +58,7 @@ func (b *Board) CanMoveDown(t *tetrimino.Tetrimino) bool {
 	return true
 }
 
+// テトリミノを回転させられるかを判定する関数
 func (b *Board) CanRotate(t *tetrimino.Tetrimino) bool {
 	rotated := t.Clone()
 	rotated.Rotate()
@@ -67,6 +73,7 @@ func (b *Board) CanRotate(t *tetrimino.Tetrimino) bool {
 	return true
 }
 
+// テトリミノがボード上に配置できるかどうかを判定する関数
 func (b *Board) CanPlace(t *tetrimino.Tetrimino) bool {
 	for _, cell := range t.Cells() {
 		x, y := cell.X, cell.Y
@@ -77,6 +84,7 @@ func (b *Board) CanPlace(t *tetrimino.Tetrimino) bool {
 	return true
 }
 
+// テトリミノをボードに結合する関数
 func (b *Board) Merge(t *tetrimino.Tetrimino) {
 	for _, cell := range t.Cells() {
 		b.Cells[cell.Y][cell.X] = true
@@ -84,15 +92,20 @@ func (b *Board) Merge(t *tetrimino.Tetrimino) {
 	b.ClearFullRows()
 }
 
+// ボード上のすべてのセルが埋まった行を削除する関数
 func (b *Board) ClearFullRows() {
+	clearedRows := 0
 	for y := b.Height - 1; y >= 0; y-- {
 		if isRowFull(b.Cells[y]) {
 			b.Cells = append(b.Cells[:y], b.Cells[y+1:]...)
 			b.Cells = append([][]bool{make([]bool, b.Width)}, b.Cells...)
+			clearedRows++
 		}
 	}
+	b.updateScore(clearedRows)
 }
 
+// 行が全て埋まっているかどうかを判断する関数
 func isRowFull(row []bool) bool {
 	for _, cell := range row {
 		if !cell {
@@ -102,8 +115,23 @@ func isRowFull(row []bool) bool {
 	return true
 }
 
+// テトリミノを下に落とす関数
 func (b *Board) Drop(t *tetrimino.Tetrimino) {
 	for b.CanMoveDown(t) {
 		t.MoveDown()
+	}
+}
+
+// スコアを更新する関数
+func (b *Board) updateScore(clearedRows int) {
+	switch clearedRows {
+	case 1:
+		b.Score += 100
+	case 2:
+		b.Score += 300
+	case 3:
+		b.Score += 500
+	case 4:
+		b.Score += 800
 	}
 }
--- main.go
+++ main.go
@@ -12,40 +12,52 @@ import (
 )
 
 func main() {
+	// termboxの初期化
 	err := termbox.Init()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to initialize termbox: %v\n", err)
 		os.Exit(1)
 	}
-	defer termbox.Close()
-
+	// ゲームボード、レンダー、現在のテトリミノを作成
 	gameBoard := board.NewBoard(10, 20)
 	render := renderer.NewRenderer()
 	currentTetrimino := tetrimino.NewRandomTetrimino()
+	//ゲーム終了時の処理
+	defer fmt.Println("Game Over!\n")
+	defer fmt.Println("Score: ", gameBoard.Score, "Point")
+	defer termbox.Close()
 
-	// Game loop
+	// ゲームループ
 	gameLoop:
 		for {
+			//　ゲームボードと現在のテトリミノを描画
 			render.DrawBoard(gameBoard)
 			render.DrawTetrimino(currentTetrimino)
-
+			// 描画内容を画面に反映
 			termbox.Flush()
 
+			//イベント処理
 			select {
 			case event := <-eventQueue():
 				switch event.Type {
 				case termbox.EventKey:
+					// Ctr+Cかqでゲーム終了
 					if event.Key == termbox.KeyCtrlC || event.Ch == 'q' {
 						break gameLoop
 					}
+					// キーイベントを処理
 					handleKeyEvent(event.Key, gameBoard, currentTetrimino)
 				}
 			case <-time.After(500 * time.Millisecond):
+				//　テトリミノを下に動かす
 				if gameBoard.CanMoveDown(currentTetrimino) {
 					currentTetrimino.MoveDown()
 				} else {
+					// テトリミノが動かせない場合、ボードに固定
 					gameBoard.Merge(currentTetrimino)
+					// 新しいテトリミノを生成
 					currentTetrimino = tetrimino.NewRandomTetrimino()
+					// ゲームオーバー判定
 					if !gameBoard.CanPlace(currentTetrimino) {
 						break gameLoop
 					}
@@ -56,6 +68,7 @@ func main() {
 	fmt.Println("Game Over!")
 }
 
+// イベントキューを作成
 func eventQueue() chan termbox.Event {
 	eventChan := make(chan termbox.Event)
 	go func() {
@@ -66,23 +79,29 @@ func eventQueue() chan termbox.Event {
 	return eventChan
 }
 
+// キーイベントの処理
 func handleKeyEvent(key termbox.Key, gameBoard *board.Board, tetrimino *tetrimino.Tetrimino) {
 	switch key {
 	case termbox.KeyArrowLeft:
+		// 左矢印キー：テトリミノを左に移動
 		if gameBoard.CanMoveLeft(tetrimino) {
 			tetrimino.MoveLeft()
 		}
 	case termbox.KeyArrowRight:
+		// 右矢印キー：テトリミノを右に移動
 		if gameBoard.CanMoveRight(tetrimino) {
 			tetrimino.MoveRight()
 		}
 	case termbox.KeyArrowDown:
+		// 下矢印キー：テトリミノを下に移動
 		if gameBoard.CanMoveDown(tetrimino) {
 			tetrimino.MoveDown()
 		}
 	case termbox.KeySpace:
+		// スペースキー：テトリミノを一気に下に落とす
 		gameBoard.Drop(tetrimino)
 	case termbox.KeyArrowUp:
+		// 上矢印キー：テトリミノを回転
 		if gameBoard.CanRotate(tetrimino) {
 			tetrimino.Rotate()
 		}
